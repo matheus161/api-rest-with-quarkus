9b2434f16f095b2fdf510f4373f68981c58bc0d6
Matheus <matheeus161@gmail.com>
2025-08-21 09:05:30 -0300

    feat: add Pessoa and PessoaResource + Panache confing and tests

---
+++ src/main/java/com/matheus161/Pessoa.java
@@ -0,0 +1,17 @@
+package com.matheus161;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+import java.util.List;
+
+@Entity
+public class Pessoa extends PanacheEntity {
+
+    public String nome;
+    public int anoNascimento;
+
+    public static List<Pessoa> findByAnoNascimento(int anoNascimento) {
+        return find("anoNascimento", anoNascimento).list();
+    }
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES (1, 'Matheus', 2001);
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES (2, 'Elder', 1979);
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES (3, 'Hugo', 1990);
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES (4, 'Isidro', 1980);
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES (5, 'Otavio', 1986);
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES (6, 'Elias', 1995);
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES (7, 'Rodrigo', 1999);
+
+ALTER SEQUENCE pessoa_seq RESTART WITH 7;
